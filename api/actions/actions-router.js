be15532c5dd0e32a1de295397e977fad73fa4f06
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-07 20:53:55 -0700

    implemented GET method for /actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,6 +3,31 @@ const express = require("express");
 const Action = require("./actions-model");
 const router = express.Router();
 
+router.get('/', async (req, res, next) =>{
+    const actions = await Action.get();
+    try {
+        res.status(200).json(actions)
+    } catch (err) {
+        next(err);
+    }
+})
+
+// router.get('/:id', (req, res) => {
+
+// })
+
+// router.post('/', (req, res) => {
+
+// })
+
+// router.put('/:id', (req,res) =>{
+
+// })
+
+// router.delete('/:id', (req, res) => {
+
+// })
+
 
 router.use((error, req, res, next) => { //eslint-disable-line
     res.status(error.status || 500).json({
