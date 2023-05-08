456192112359aebf49cb2782fe4127b61de1025b
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-07 18:39:14 -0700

    wired up first projects route for GET

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,14 @@
 // Write your "actions" router here!
+const express = require("express");
+// const Action = require("./actions-model");
+const router = express.Router();
+
+
+router.use((error, req, res, next) => { //eslint-disable-line
+    res.status(error.status || 500).json({
+        message: error.message,
+        customMessage: "something bad happened in the actions router"
+    })
+})
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,43 @@
 // Write your "projects" router here!
+const express = require("express");
+const Project = require("./projects-model");
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await Project.get();
+    res.status(200).json(projects);
+  } catch (err) {
+    err(next);
+  }
+});
+
+// router.get('/:id', (req, res) => {
+
+// })
+
+// router.post('/', (req, res) => {
+
+// })
+
+// router.put('/:id', (req, res) => {
+
+// })
+
+// router.delete('/:id', (req, res) => {
+
+// })
+
+// router.get('/:id/actions', (req, res) => {
+
+// })
+
+router.use((error, req, res, next) => {
+  //eslint-disable-line
+  res.status(error.status || 500).json({
+    message: error.message,
+    customMessage: "something bad happened in the projects router",
+  });
+});
+
+module.exports = router;
