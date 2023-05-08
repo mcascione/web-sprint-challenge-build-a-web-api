ce94be9fbf16e2bd41ff1e0153c0e7ebffb40b80
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-07 21:47:30 -0700

    implemented delete method for /actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,60 +1,66 @@
 // Write your "actions" router here!
 const express = require("express");
 const Action = require("./actions-model");
-const { validateActionID, validateAction } = require('./actions-middleware');
+const { validateActionID, validateAction } = require("./actions-middleware");
 const router = express.Router();
 
-router.get('/', async (req, res, next) =>{
-    const actions = await Action.get();
-    try {
-        res.status(200).json(actions)
-    } catch (err) {
-        next(err);
-    }
-})
+router.get("/", async (req, res, next) => {
+  const actions = await Action.get();
+  try {
+    res.status(200).json(actions);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get('/:id', validateActionID,  async(req, res, next) => {
-    const result = await Action.get(req.params.id);
-    try {
-        res.status(200).json(result);
-    } catch (err) {
-        next(err);
-    }
-})
+router.get("/:id", validateActionID, async (req, res, next) => {
+  const result = await Action.get(req.params.id);
+  try {
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post('/', validateAction, async (req, res, next) => {
-    try {
-        const action = await Action.insert({
-            project_id : req.project_id,
-            description: req.description,
-            notes: req.notes,
-            completed: req.completed
-         });
-       res.status(201).json(action);
-    } catch (err) {
-        next(err);
-    }
-})
+router.post("/", validateAction, async (req, res, next) => {
+  try {
+    const action = await Action.insert({
+      project_id: req.project_id,
+      description: req.description,
+      notes: req.notes,
+      completed: req.completed,
+    });
+    res.status(201).json(action);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.put('/:id', validateActionID, validateAction, async (req,res, next) =>{
-    const updatedAction = await Action.update(req.params.id, req.body);
-    try {
-        res.status(200).json(updatedAction);
-    } catch (err){
-        next(err);
-    }
-})
+router.put("/:id", validateActionID, validateAction, async (req, res, next) => {
+  const updatedAction = await Action.update(req.params.id, req.body);
+  try {
+    res.status(200).json(updatedAction);
+  } catch (err) {
+    next(err);
+  }
+});
 
-// router.delete('/:id', validateActionsId, (req, res, next) => {
+router.delete("/:id", validateActionID, (req, res, next) => {
+  Action.remove(req.params.id)
+    .then(
+      res.status(200).json({
+        message: "deleted action",
+      })
+    )
+    .catch(next);
+});
 
-// })
-
-
-router.use((error, req, res, next) => { //eslint-disable-line
-    res.status(error.status || 500).json({
-        message: error.message,
-        customMessage: "something bad happened in the actions router"
-    })
-})
+//eslint-disable-next-line
+router.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message,
+    customMessage: "something bad happened in the actions router",
+  });
+});
 
 module.exports = router;
